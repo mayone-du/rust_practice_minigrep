5a05e29362fc6c38d6a065f1f119cd9552727ab0
mayone-du <cocomayo1201@icloud.com>
2021-08-04 09:58:41 +0900

    Add search, test

--- src/lib.rs
+++ src/lib.rs
@@ -35,8 +35,45 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
   // ファイルの内容を↑の可変文字列にバインド
   f.read_to_string(&mut contents)?;
 
-  println!("With text:\n{}", contents);
-
+  // 検索し出力する
+  for line in search(&config.query, &contents) {
+    println!("{}", line);
+  }
   // うまく行ったらResultのOkを返す
   Ok(())
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    // Rustは
+    // 安全で速く生産性も高い。
+    // 3つ選んで。
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+  }
+}
+
+// ライフタイムを指定
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  // 返り値用のベクター型変数
+  let mut results = Vec::new();
+
+  // ファイルの内容を1行ずつforループで回し、各行内に検索したい文字列が含まれているかを確認
+  for line in contents.lines() {
+    // 含まれていたら、その行をベクターにpush
+    if line.contains(query) {
+      results.push(line);
+    }
+  }
+
+  results
 }
